ca1c58fcead1e73bb02ddc9eaf06489de039c4a0
Bishko <yurik322412@gmail.com>
2021-08-05 20:13:25 +0300

    Updates

---
+++ precache-manifest.32c0df65c765ca9bbfa570cc980dc4f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f34b22b443ba35ea82ed57a45aa7d1e5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7ef52b866b0115b11e65",
+    "url": "./static/css/main.8c8e3dd9.chunk.css"
+  },
+  {
+    "revision": "da84d8a56cf01ffef8a6",
+    "url": "./static/js/2.302f5aa1.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.302f5aa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ef52b866b0115b11e65",
+    "url": "./static/js/main.3645be13.chunk.js"
+  },
+  {
+    "revision": "34f8ca02e3faf0884ac1",
+    "url": "./static/js/runtime-main.c37fc973.js"
+  }
+]);
